1ac63e859307ccfa621d7e0df37eaa0400fbbc25
Antonio <tonyestrada658@gmail.com>
2023-07-03 07:48:29 -0600

    first commit

---
+++ src/app/tab3/tab3.page.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab3',
+  templateUrl: 'tab3.page.html',
+  styleUrls: ['tab3.page.scss']
+})
+
+export class Tab3Page {
+
+  public actionSheetButtons = [
+    {
+      text: 'Delete',
+      role: 'destructive',
+      data: {
+        action: 'delete',
+      },
+    },
+    {
+      text: 'Cancel',
+      role: 'cancel',
+      data: {
+        action: 'cancel',
+      },
+    },
+  ];
+
+  constructor() {}
+
+}
